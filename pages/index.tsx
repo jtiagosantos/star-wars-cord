6b3545e3175efc87c634d2398b5f173cc488fbec
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-01-25 10:58:48 -0300

    chore: add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import { GlobalStyles } from '../src/styles/GlobalStyles';
 
+import { Main, ProfileCard, Auth, PhotoProfile } from '../src/styles/home-styles';
+
 export default function Home() {
+  const [username, setUsername] = useState('jtiagosantos');
+
   return (
     <>
       <GlobalStyles />
@@ -9,6 +14,29 @@ export default function Home() {
       <Head>
         <title>Code Cord</title>
       </Head>
+
+      <Main>
+        <ProfileCard>
+          <Auth>
+            <h2>Boas vindas!</h2>
+            <p>Code Cord 💜🚀</p>
+            <section>
+              <input 
+                type="text" 
+                placeholder="seu usuário do Github" 
+                value={username}
+                onChange={({ target }) => setUsername(target.value)}
+              />
+              <button type="button">Entrar</button>
+            </section>
+          </Auth>
+
+          <PhotoProfile>
+            <img src={`https://github.com/${username}.png`} />
+            <p>jtiagosantos</p>
+          </PhotoProfile>
+        </ProfileCard>
+      </Main>
     </>
   )
 }
---
+++ src/styles/home-styles.ts
@@ -0,0 +1,114 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ProfileCard = styled.div`
+  background: #212931;
+  box-shadow: 0px 2px 10px rgba(255, 255, 255, 0.2);
+  border-radius: 5px;
+  padding: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 120px;
+
+  @media(max-width: 745px) {
+    flex-direction: column-reverse;
+    gap: 50px;
+  }
+`;
+
+export const Auth = styled.div`
+  h2 {
+    text-align: center;
+    color: #ffffff;
+    font-weight: 700;
+
+    margin-bottom: 5px;
+  }
+
+  p {
+    text-align: center;
+    color: #9AA5B1;
+    font-weight: 700;
+    font-size: 0.875rem;
+
+    margin-bottom: 50px;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    width: 320px;
+
+    @media(max-width: 400px) {
+      width: 250px;
+    }
+
+    input {
+      width: 100%;
+      background: #181F25;
+      border: 1px solid #080A0C;
+      border-radius: 4px;
+      height: 34px;
+      padding: 4px;
+
+      color: #ffffff;
+    }
+
+    button {
+      background: #7768df;
+      border: none;
+      border-radius: 4px;
+      padding: 10px 0;
+      
+      color: #ffffff;
+      font-size: 14px;
+
+      transition: filter 0.2s;
+
+      &:hover {
+        filter: brightness(0.8);
+      }
+    }
+  }
+`;
+
+export const PhotoProfile = styled.div`
+  width: 200px;
+  height: 240px;
+  background: #181F25;
+  border: 1px solid #080A0C;
+  border-radius: 10px;
+  padding: 16px 22px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 150px;
+    border-radius: 1000px;
+  }
+
+  p {
+    margin-top: 22px;
+    background: #080A0C;
+    border-radius: 100px;
+    padding: 3px 10px;
+
+    color: #CBD2D9;
+    font-size: 0.75rem;
+    font-weight: 700;
+  }
+`;
