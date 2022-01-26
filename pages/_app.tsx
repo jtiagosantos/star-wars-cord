dd36369448d4c6d3588381c183c6055bd6464125
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-01-26 00:59:30 -0300

    chore: add react query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import { GlobalStyles } from '../src/styles/GlobalStyles';
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   ); 
 }
 
