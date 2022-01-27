e1a0fce50d3c5a6b97842033ae0e558c24ab9654
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-01-27 19:57:33 -0300

    feat: change order

--- src/services/supabase/get-messages.ts
+++ src/services/supabase/get-messages.ts
@@ -2,7 +2,9 @@ import { supabase } from './supabase-client';
 import { Message } from '../../types/message';
 
 export async function getMessagesService() {
-  const { data, error } = await supabase.from('messages').select('*');
+  const { data, error } = await supabase.from('messages').select('*').order('id', {
+    ascending: false,
+  });
 
   if (error) {
     throw error;
