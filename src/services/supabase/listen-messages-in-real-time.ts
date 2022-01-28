e85bae4c4218fc03a72325ef54b4151c637c8f6c
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-01-28 16:56:01 -0300

    feat: add messages in real time service

---
+++ src/services/supabase/listen-messages-in-real-time.ts
@@ -0,0 +1,32 @@
+import { Dispatch, SetStateAction } from 'react';
+import { supabase } from './supabase-client';
+import { Message } from '../../types/message';
+
+export function listenMessagesInRealTimeService(
+  messageList: Message[],
+  setMessageList: Dispatch<SetStateAction<Message[]>>
+) {
+  return supabase.from('messages').on('*', (data) => {
+    if (data.eventType === 'INSERT') {
+
+      setMessageList(() => {
+        return [
+          data.new,
+          ...messageList,
+        ];
+      });
+
+      return
+    }
+
+    if (data.eventType === 'DELETE') {
+
+      setMessageList(() => {
+        return [
+          ...messageList,
+        ];
+      });
+    }
+
+  }).subscribe();
+}
