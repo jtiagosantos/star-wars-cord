6be528876c9c0bad111bdc875a92e1b715298b3d
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2022-01-28 12:29:00 -0300

    feat: add stickers card compoent

---
+++ src/components/StickersCard/StickersCard.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { stickers } from '../../constants/stickers';
+
+import { Container, StickersList } from './styles';
+
+interface StickersCardProps {
+  onSendStickerMessage: (sticker: string) => void;
+  isLoadingSendMessage: boolean;
+}
+
+export function StickersCard({ 
+  onSendStickerMessage,
+  isLoadingSendMessage,
+}: StickersCardProps) {
+  const [isOpenStickersCard, setIsOpenStickersCard] = useState(false);
+
+  function handleOpenAndCloseStickersCard() {
+    setIsOpenStickersCard(isOpenStickersCard ? false : true);
+  }
+
+  function handleSendStickerMessage(sticker: string) {
+    onSendStickerMessage(sticker);
+    handleOpenAndCloseStickersCard();
+  }
+
+  return (
+    <Container>
+      <button 
+        type="button" 
+        onClick={handleOpenAndCloseStickersCard}
+      >
+        ⚡
+      </button>
+
+      {isOpenStickersCard && (
+        <StickersList>
+        {stickers.map((sticker, index) => (
+          <img 
+            key={index} 
+            src={sticker} 
+            onClick={
+              () => handleSendStickerMessage(sticker)
+            }
+          />
+        ))}
+      </StickersList>
+      )}
+    </Container>
+  );
+}
---
+++ src/components/StickersCard/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+
+  button {
+    background: #9AA5B1;
+    width: 42px;
+    height: 43px;
+  }
+`;
+
+export const StickersList = styled.div`
+  position: absolute;
+  padding: 16px;
+  right: 30px;
+  bottom: 30px;
+  background: #171b1f;
+  border-radius: 5px;
+  width: 258px;
+  height: 256px;
+
+  
+  display: flex;
+  flex-wrap: wrap;
+  gap: 7px;
+
+  overflow-y: auto;
+
+  ::-webkit-scrollbar {
+      width: 2px;
+    }
+    
+  ::-webkit-scrollbar-thumb {
+    background-color: #ccc;
+    border-radius: 5px;
+  }
+
+  img {
+    width: 70px;
+    height: 70px;
+    border: 1px solid #0f1012;
+    border-radius: 5px;
+    cursor: pointer;
+
+    &:hover {
+      border-color: #d8f500;
+    }
+  }
+`;
